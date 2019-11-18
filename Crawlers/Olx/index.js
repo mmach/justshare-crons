d4f37e56611dc2bac1eb7dea4dabec494dc71075
mmach900 <m.mach.90@gmail.com>
2019-11-18 14:51:47 +0100

    no message

--- Crawlers/Olx/index.js
+++ Crawlers/Olx/index.js
@@ -4,13 +4,9 @@ var parseString = require('xml2js').parseString;
 const amqp = require('amqplib/callback_api');
 let url = [];
 let urlItems = [];
-let urlSubregions = []; 
-module.exports = (access) => {
-    if (access == 'get') {
-        return urlItems
-    } else {
-        urlItems = [];
-    }
+let urlSubregions = [];
+let load_categories = (pool,sql) => {
+
 
     var c_sitemap_regions = new Crawler({
         maxConnections: 1,
@@ -77,7 +73,7 @@ module.exports = (access) => {
                     done();
                 }, 60000)
             } else {
-                parseString(res.body, function (err, result) {
+                parseString(res.body,async  function (err, result) {
                     if (result == undefined) {
                         console.log(res)
                         done();
@@ -86,61 +82,37 @@ module.exports = (access) => {
                     const CONN_URL = 'amqp://kyqjanjv:6djuPiJWnpZnIMT1jZ-SvIULv8IOLw2P@hedgehog.rmq.cloudamqp.com/kyqjanjv';
                     let ch = null;
 
-                    amqp.connect(CONN_URL, function (err, conn) {
-                        if (err) {
-                            console.log("CONNECTION ERROR");
 
-                            console.log(err);
-                            setTimeout(() => {
-                                done();
-                                conn.close();
 
-                            }, 60000)
-                            return;
+                    let items = result.urlset.url.map(async item => {
+
+                        if (item.loc[0].includes('nieruchomosci')
+                            || item.loc[0].includes('dla-dzieci')) {
+
+
+                            console.log(item.loc[0])
+                            //  let queue = await addToQueue();
+                            //   console.log(queue);
+                            //queue.forEach(item => {
+
+                            //  });
+                            //  console.log(test);
+                            //  ch.close();
+
+                            return await pool.request().input('sitemap_link',sql.Text, item.loc[0]).input('integration_name', sql.Text, 'OLX_PL').execute(`INSERT_Sitemap`)
+
+
+
                         }
-                        conn.createChannel(async function (err2, channel) {
-                            if (err2) {
-                                console.log("CHANEL ERROR");
-
-                                console.log(err);
-                                setTimeout(() => {
-                                    done();
-                                }, 60000)
-                                return
-                                // throw err2;
-                            } ch = channel;
-                            channel.assertQueue('olx-sitemap-crawler', {
-                                durable: true
-                            });
-                            result.urlset.url.map(item => {
-
-                                if (item.loc[0].includes('nieruchomosci')
-                                    || item.loc[0].includes('dla-dzieci')) {
-
-
-                                    console.log(item.loc[0])
-                                    //  let queue = await addToQueue();
-                                    //   console.log(queue);
-                                    //queue.forEach(item => {
-                                    ch.sendToQueue('olx-sitemap-crawler', new Buffer(item.loc[0]), { persistent: true });
-                                    //  });
-                                    //  console.log(test);
-                                    //  ch.close();
-
-
-
-
-                                }
-                            })
-                        });
-
-                        setTimeout(() => {
-                            conn.close();
-                            done();
-
-                        }, 5000)
                     })
-                })
+                    await Promise.all(items)
+                });
+
+                setTimeout(() => {
+                    done();
+
+                }, 5000)
+
             }
         }
     }
@@ -163,186 +135,68 @@ module.exports = (access) => {
         skipDuplicates: true
 
     })
-
-
-
-
-    var c = new Crawler({
-        maxConnections: 1,
-        rateLimit: 2000,
-
-        // This will be called for each crawled page
-        preRequest: function (options, done) {
-            done();
-
-        },
-        callback: async function (error, res, done) {
-            if (error) {
-                console.log(error);
-                done();
-            } else {
-                var $ = res.$;
-                // $ is Cheerio by default
-                //a lean implementation of core jQuery designed specifically for the server
-                // console.log($.html())
-                //     console.log($('a[data-cy="page-link-next"]'));
-                let offer = $('table[data-id]')
-                let stopCrawling = false;
-                let itemsToSend = [];
-
-                Object.keys(offer).filter(item => {
-                    return isNaN(item) == false
-                }).map(item => {
-                    let tbody = offer[item].children.filter(el => { return el.name == 'tbody' })[0];
-                    let tr = tbody.children.filter(el => { return el.name == 'tr' });
-                    //console.log(tr)
-                    tr.map(trElement => {
-                        //console.log(trElement)
-                        trElement.children.filter(el => {
-                            if (el.name == 'td' && el.attribs.valign == 'bottom' && el.attribs.class == 'bottom-cell') {
-                                el.children.filter(dataEl => {
-                                    if (dataEl.name == 'div') {
-                                        dataEl.children.filter(p => {
-                                            if (p.name == 'p') {
-                                                p.children.filter(small => {
-                                                    if (small.name == 'small') {
-                                                        small.children.filter(span => {
-                                                            if (span.name == 'span') {
-                                                                if (span.children.filter(i => {
-                                                                    return i.name == 'i' && i.attribs["data-icon"] == "clock"
-                                                                }).length > 0) {
-                                                                    if (!span.children[2].data.includes('dzisiaj')) {
-                                                                        stopCrawling = true;
-                                                                    } else {
-                                                                        tr.map(trChildren => {
-                                                                            trChildren.children.map(td => {
-                                                                                if (td.name == 'td' && td.attribs.rowspan == '2') {
-
-                                                                                    td.children.forEach(a => {
-
-                                                                                        if (a.name == 'a') {
-
-
-                                                                                            if (url.filter(urlFilter => {
-                                                                                                return urlFilter == a.attribs.href
-                                                                                            }).length > 0) {
-                                                                                                //  done();
-                                                                                                return;
-                                                                                            } else {
-                                                                                                if (!a.attribs.href.includes('https://www.olx.pl')) {
-                                                                                                    return
-                                                                                                }
-                                                                                                console.log(a.attribs.href)
-                                                                                                console.log(span.children[2].data)
-                                                                                                urlItems.push(a.attribs.href)
-                                                                                                itemsToSend.push(a.attribs.href);
-                                                                                                console.log(urlItems.length)
-
-                                                                                                //     c_items.queue({
-                                                                                                //         uri: a.attribs.href,
-                                                                                                //         forceUTF8: false,
-                                                                                                //         headers: {
-                                                                                                //              "Content-Type": "application/json",
-                                                                                                //              "sec-fetch-site": "same-origin",
-                                                                                                //              "sec-fetch-mode": "navigate",
-                                                                                                //              "sec-fetch-user": "?1",
-                                                                                                //              "upgrade-insecure-requests": 1,
-                                                                                                //              "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
-
-
-                                                                                                //           }
-
-                                                                                                //      })
-                                                                                            }
-                                                                                        }
-                                                                                    })
-                                                                                }
-                                                                            });
-                                                                        })
-                                                                    }
-                                                                    //console.log(span.children[2].data.includes('dzisiaj'))
-                                                                }
-                                                            }
-                                                        })
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }
-                                })
-                            }
-                        })
-                    })
-                });
-                const CONN_URL = 'amqp://kyqjanjv:6djuPiJWnpZnIMT1jZ-SvIULv8IOLw2P@hedgehog.rmq.cloudamqp.com/kyqjanjv';
-                let ch = null;
-                await new Promise((res, rej) => {
-                    amqp.connect(CONN_URL, function (err, conn) {
-                        if (err) {
-                            console.log(err);
-                            return
-                        }
-                        conn.createChannel(async function (err2, channel) {
-                            if (err2) {
-                                console.log(err2);
-                                return
-                            } ch = channel;
-                            channel.assertQueue('olx-link-items', {
-                                durable: true
-                            });
-
-                            //  let queue = await addToQueue();
-                            //   console.log(queue);
-                            //queue.forEach(item => {
-                            ch.sendToQueue('olx-link-items', new Buffer(JSON.stringify(itemsToSend)), { persistent: true });
-                            //  });
-                            //  console.log(test);
-                            res();
-                            //  ch.close();
-                            done();
-                        });
-
-                        setTimeout(() => {
-                            conn.close();
-
-                        }, 1000)
-                    });
-                });
-                if (stopCrawling == true) {
-                    done();
-                    return;
-                }
-                if (url.filter(item => {
-                    return item == $('a[data-cy="page-link-next"]').attr('href')
-                }).length > 0) {
-                    done();
-                    return;
-                } else {
-                    url.push($('a[data-cy="page-link-next"]').attr('href'));
-                }
-                c.queue({
-                    uri: $('a[data-cy="page-link-next"]').attr('href'),
-                    forceUTF8: false,
-                    headers: {
-                        "Content-Type": "application/json",
-                        "sec-fetch-site": "same-origin",
-                        "sec-fetch-mode": "navigate",
-                        "sec-fetch-user": "?1",
-                        "upgrade-insecure-requests": 1,
-                        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
-
-
-                    }
-
-                })
-            }
-            done();
-        }
-    });
-
-
-    // Queue just one URL, with default callback
-
-
     // Queue some HTML code directly without grabbing (mostly for tests)
+}
+
+
+let start_crawler = async (pool,sql) => {
+    const CONN_URL = 'amqp://kyqjanjv:6djuPiJWnpZnIMT1jZ-SvIULv8IOLw2P@hedgehog.rmq.cloudamqp.com/kyqjanjv';
+    let ch = null;
+
+    const result_sitemap = await pool.query` SELECT * FROM Sitemap
+    WHERE integration_id IN ( SELECT id FROM Integration Where Name='OLX_PL')`;
+    amqp.connect(CONN_URL, function (err, conn) {
+        if (err) {
+            console.log("CONNECTION ERROR");
+
+            console.log(err);
+            setTimeout(() => {
+                done();
+                conn.close();
+
+            }, 60000)
+            return;
+        }
+        conn.createChannel(async function (err2, channel) {
+            if (err2) {
+                console.log("CHANEL ERROR");
+
+                console.log(err);
+                setTimeout(() => {
+                    done();
+                }, 60000)
+                return
+                // throw err2;
+            } ch = channel;
+            channel.assertQueue('olx-sitemap-crawler', {
+                durable: true
+            });
+
+            result_sitemap.recordset.map(async item => {
+
+
+
+                console.log(item.link)
+                //  let queue = await addToQueue();
+                //   console.log(queue);
+                //queue.forEach(item => {
+
+                ch.sendToQueue('olx-sitemap-crawler', new Buffer(item.link), { persistent: true });
+                //  });
+                //  console.log(test);
+                //  ch.close();
+
+            })
+        });
+
+        setTimeout(() => {
+            conn.close();
+            done();
+
+        }, 60000)
+    })
+}
+module.exports = {
+    load_categories,start_crawler
+
 }
--- index.js
+++ index.js
@@ -2,40 +2,77 @@ var CronJob = require('cron').CronJob;
 var amqp = require('amqplib/callback_api');
 var axios = require('axios');
 var olx = require('./Crawlers/Olx/index.js')
+const sql = require('mssql')
 
-new CronJob('1 */2 * * * *', function () {
-    console.log('Run integration');
-    axios({
-        method: 'post',
-        url: `https://stuffshare-web.herokuapp.com/command`,
-        data: {
-            "action": "syncItemCommand"
-            , "model": {}
+sql.connect('mssql://admin:justshare123@justshare-integration.clrcukgqxhwe.us-east-2.rds.amazonaws.com/IntegrationDB').then(pool => {
+
+
+
+    new CronJob('1 */2 * * * *', function () {
+        console.log('Run integration');
+        axios({
+            method: 'post',
+            url: `https://stuffshare-web.herokuapp.com/command`,
+            data: {
+                "action": "syncItemCommand"
+                , "model": {}
+            }
+        }).then(succ3 => {
+            console.log(" [x] RUN SYNC DONE");
+
+        }, err3 => {
+            console.log(err3);
+        })
+
+
+
+
+    }, null, true, 'America/Los_Angeles');
+
+
+
+
+    new CronJob('1 0 */10 * * *', async function () {
+
+        try {
+            // make sure that any items are correctly URL encoded in the connection string
+
+            const result = await pool.query`select * from Integration WHERE IsActive=1`;
+            console.dir(result)
+            let promisesList = result.recordset.map(async item => {
+
+                if (item.Name == 'OLX_PL') {
+                    return await olx.load_categories(pool,sql)
+                }
+            });
+            await Promise.all(promisesList)
+        } catch (err) {
+            console.log(err);
+            // ... error checks
         }
-    }).then(succ3 => {
-        console.log(" [x] RUN SYNC DONE");
-
-    }, err3 => {
-        console.log(err3);
-    })
 
 
 
-
-}, null, true, 'America/Los_Angeles');
-
+    }, null, true, null, null, true);
 
 
+    new CronJob('1 0 */3 * * *', async function () {
+        try {
+            // make sure that any items are correctly URL encoded in the connection string
+            const result = await pool.query`select * from Integration WHERE IsActive=1`;
+            console.dir(result)
+            let promisesList = result.recordset.map(async item => {
 
-new CronJob('1 0 */10 * * *', function () {
-    olx()
+                if (item.Name == 'OLX_PL') {
+                    return await olx.start_crawler(pool,sql)
 
+                }
+            });
+            await Promise.all(promisesList)
+        } catch (err) {
+            console.log(err);
+            // ... error checks
+        }
 
-}, null, true, null, null, true);
-
-
-new CronJob('1 0 */3 * * *', function () {
-    let cron = olx('get')
-    console.log(cron);
-
-}, null, true);
+    }, null, true, null, null, true);
+})
